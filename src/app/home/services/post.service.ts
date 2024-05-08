f161f2ef484b343310364a85f2ed8864a3687493
AlexLeoChumak <chumak1106@gmail.com>
2024-05-08 20:15:30 +0300

    create PostService

---
+++ src/app/home/services/post.service.ts
@@ -0,0 +1,14 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Post } from '../models/post.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PostService {
+  constructor(private http: HttpClient) {}
+
+  getSelectedPosts(params: string) {
+    return this.http.get<Post[]>('http://localhost:3000/api/feed' + params);
+  }
+}

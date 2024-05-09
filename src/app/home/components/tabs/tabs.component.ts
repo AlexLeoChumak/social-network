389d5b2ea1ec24dd1e8b1ba3bbd84b0a2a53e2fe
AlexLeoChumak <chumak1106@gmail.com>
2024-05-09 17:25:01 +0300

    create TabsComponent

---
+++ src/app/home/components/tabs/tabs.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { ModalComponent } from '../modal/modal.component';
+
+@Component({
+  selector: 'app-tabs',
+  templateUrl: './tabs.component.html',
+  styleUrls: ['./tabs.component.scss'],
+})
+export class TabsComponent implements OnInit {
+  constructor(public modalController: ModalController) {}
+
+  ngOnInit() {}
+
+  async presentModal() {
+    const modal = await this.modalController.create({
+      component: ModalComponent,
+      cssClass: 'my-custom-class-modal-post',
+    });
+    await modal.present();
+    const { data, role } = await modal.onDidDismiss();
+
+    console.log('role: ', role, 'data:', data);
+  }
+}

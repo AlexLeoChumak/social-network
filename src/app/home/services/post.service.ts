14ab7acb4be816ec59b781bf75b0cc6f61f50d02
AlexLeoChumak <chumak1106@gmail.com>
2024-05-09 16:09:41 +0300

    add /pagination to URL

--- src/app/home/services/post.service.ts
+++ src/app/home/services/post.service.ts
@@ -9,6 +9,8 @@ export class PostService {
   constructor(private http: HttpClient) {}
 
   getSelectedPosts(params: string) {
-    return this.http.get<Post[]>('http://localhost:3000/api/feed' + params);
+    return this.http.get<Post[]>(
+      'http://localhost:3000/api/feed/pagination' + params
+    );
   }
 }

583a30319e3e62628e50d653d20bbfb3bc9014c5
AlexLeoChumak <chumak1106@gmail.com>
2024-05-14 11:12:01 +0300

    create get isUserLoggedIn(), private user$

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,12 +1,20 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { Observable, catchError, tap, throwError } from 'rxjs';
+import {
+  BehaviorSubject,
+  Observable,
+  catchError,
+  of,
+  switchMap,
+  tap,
+  throwError,
+} from 'rxjs';
 import { Preferences } from '@capacitor/preferences';
 import { jwtDecode } from 'jwt-decode';
 
 import { NewUser } from '../models/newUser.interface';
-import { User } from '../models/user.interface';
+import { Role, User } from '../models/user.interface';
 import { environment } from 'src/environments/environment';
 import { UserResponse } from '../models/userResponse.interface';
 
@@ -14,11 +22,22 @@ import { UserResponse } from '../models/userResponse.interface';
   providedIn: 'root',
 })
 export class AuthService {
+  private user$ = new BehaviorSubject<UserResponse | null>(null);
+
   private httpOptions: { headers: HttpHeaders } = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
 
   constructor(private http: HttpClient, private router: Router) {}
+
+  get isUserLoggedIn(): Observable<boolean> {
+    return this.user$.asObservable().pipe(
+      switchMap((userResponse: UserResponse | null) => {
+        const isUserAuthenticated = userResponse !== null;
+        return of(isUserAuthenticated);
+      })
+    );
+  }
 
   registerUser(newUser: NewUser): Observable<User> {
     return this.http
@@ -49,7 +68,7 @@ export class AuthService {
           });
 
           const decodedToken: UserResponse = jwtDecode(response.token);
-          console.log('decodedToken', decodedToken);
+          this.user$.next(decodedToken);
         }),
         catchError((err) => {
           console.error(err);

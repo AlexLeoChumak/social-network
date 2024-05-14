03e07907caa20fbeab4d46dee6f72d1e6639a393
AlexLeoChumak <chumak1106@gmail.com>
2024-05-14 13:50:06 +0300

    change logic without Subscribes

--- src/app/auth/guards/auth.guard.ts
+++ src/app/auth/guards/auth.guard.ts
@@ -1,5 +1,31 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  GuardResult,
+  MaybeAsync,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
-export const authGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): MaybeAsync<GuardResult> {
+    const isUserLoggedIn = this.authService.isUserLoggedIn;
+
+    if (!isUserLoggedIn) {
+      this.router.navigate(['/auth']);
+      return false;
+    }
+
+    return true;
+  }
+}

032e35516f9252288a1c8cd7a8cf270a3ee4c320
AlexLeoChumak <chumak1106@gmail.com>
2024-05-17 16:56:40 +0300

    create AuthInterceptorService

---
+++ src/app/auth/services/auth-interceptor.service.ts
@@ -0,0 +1,41 @@
+import {
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
+} from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Preferences } from '@capacitor/preferences';
+import { Observable, from, switchMap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthInterceptorService implements HttpInterceptor {
+  constructor() {}
+
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    return from(
+      Preferences.get({
+        key: 'token',
+      })
+    ).pipe(
+      switchMap((data: { value: string | null }) => {
+        const token = data?.value;
+
+        if (token) {
+          const cloneRequest = req.clone({
+            headers: req.headers.set('Authorization', `Bearer ${token}`),
+          });
+
+          return next.handle(cloneRequest);
+        }
+
+        return next.handle(req);
+      })
+    );
+  }
+}

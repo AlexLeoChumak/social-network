d6fff1c8f225f1c93f3ffcbb620efd1485536729
AlexLeoChumak <chumak1106@gmail.com>
2024-05-18 15:50:40 +0300

    add loadNewPost(), ngOnDestroy()

--- src/app/home/components/all-posts/all-posts.component.ts
+++ src/app/home/components/all-posts/all-posts.component.ts
@@ -1,48 +1,72 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { IonInfiniteScrollCustomEvent } from '@ionic/core';
+import { IonInfiniteScroll } from '@ionic/angular';
+import { Subscription } from 'rxjs';
+
 import { PostService } from '../../services/post.service';
 import { Post } from '../../models/post.interface';
-// import { IonInfiniteScroll } from '@ionic/angular';
 
 @Component({
   selector: 'app-all-posts',
   templateUrl: './all-posts.component.html',
   styleUrls: ['./all-posts.component.scss'],
 })
-export class AllPostsComponent implements OnInit {
-  // @ViewChild(IonInfiniteScroll) infiniteScroll!: IonInfiniteScroll;
+export class AllPostsComponent implements OnInit, OnDestroy {
+  @ViewChild(IonInfiniteScroll) infiniteScroll!: IonInfiniteScroll;
 
   queryParams: string = '';
   allLoadedPosts: Post[] = [];
   numberOfPosts: number = 5;
   skipPosts: number = 0;
+  getSelectedPostsSub!: Subscription;
+  getPostBehaviorSubjectSub!: Subscription;
 
   constructor(private postService: PostService) {}
 
   ngOnInit() {
     this.loadData(null);
+    this.loadNewPost();
+  }
+
+  loadNewPost() {
+    this.getPostBehaviorSubjectSub = this.postService
+      .getPostBehaviorSubject()
+      .subscribe({
+        next: (newPost) =>
+          newPost ? this.allLoadedPosts.unshift(newPost) : null,
+        error: (err) => console.error(err), //add notification for user
+      });
   }
 
   loadData(event: IonInfiniteScrollCustomEvent<void> | null): void {
     this.queryParams = `?take=${this.numberOfPosts}&skip=${this.skipPosts}`;
 
-    this.postService.getSelectedPosts(this.queryParams).subscribe({
-      next: (posts: Post[]) => {
-        if (posts.length === 0 || posts.length < this.numberOfPosts) {
-          event ? (event.target.disabled = true) : null;
+    this.getSelectedPostsSub = this.postService
+      .getSelectedPosts(this.queryParams)
+      .subscribe({
+        next: (posts: Post[]) => {
+          for (let post = 0; post < posts.length; post++) {
+            this.allLoadedPosts.push(posts[post]);
+          }
+
+          if (posts.length === 0 || posts.length < this.numberOfPosts) {
+            event ? (event.target.disabled = true) : null;
+            event ? event.target.complete() : null;
+            return;
+          }
+
+          this.skipPosts += this.numberOfPosts;
+
           event ? event.target.complete() : null;
-          return;
-        }
+        },
+        error: (err) => console.log(err),
+      });
+  }
 
-        for (let post = 0; post < posts.length; post++) {
-          this.allLoadedPosts.push(posts[post]);
-        }
-
-        this.skipPosts += this.numberOfPosts;
-
-        event ? event.target.complete() : null;
-      },
-      error: (err) => console.log(err),
-    });
+  ngOnDestroy(): void {
+    this.getSelectedPostsSub ? this.getSelectedPostsSub.unsubscribe() : null;
+    this.getPostBehaviorSubjectSub
+      ? this.getPostBehaviorSubjectSub.unsubscribe()
+      : null;
   }
 }

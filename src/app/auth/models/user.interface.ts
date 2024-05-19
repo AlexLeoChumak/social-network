c3ee68cbc31320d0f49cf005672a93d455995322
AlexLeoChumak <chumak1106@gmail.com>
2024-05-19 18:27:54 +0300

    changed value for ID

--- src/app/auth/models/user.interface.ts
+++ src/app/auth/models/user.interface.ts
@@ -3,7 +3,7 @@ import { Post } from 'src/app/home/models/post.interface';
 export type Role = 'admin' | 'premium' | 'user';
 
 export interface User {
-  id: string;
+  id: number;
   firstName: string;
   lastName: string;
   email: string;

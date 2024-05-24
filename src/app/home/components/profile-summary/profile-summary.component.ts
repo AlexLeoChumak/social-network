bbb98ce7df731e3307bc8ff7a63dad03448bb1dd
AlexLeoChumak <chumak1106@gmail.com>
2024-05-24 16:12:08 +0300

    add form, validation types, ngOnDestroy

--- src/app/home/components/profile-summary/profile-summary.component.ts
+++ src/app/home/components/profile-summary/profile-summary.component.ts
@@ -1,7 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Subscription } from 'rxjs';
 
 import { Role } from 'src/app/auth/models/user.interface';
 import { AuthService } from 'src/app/auth/services/auth.service';
+
+type ValidFileExtension = 'png' | 'jpg' | 'jpeg';
+type ValidMimeType = 'image/png' | 'image/jpg' | 'image/jpeg';
 
 type BannerColors = {
   colorOne: string;
@@ -14,19 +19,35 @@ type BannerColors = {
   templateUrl: './profile-summary.component.html',
   styleUrls: ['./profile-summary.component.scss'],
 })
-export class ProfileSummaryComponent implements OnInit {
+export class ProfileSummaryComponent implements OnInit, OnDestroy {
   bannerColors: BannerColors = {
     colorOne: '#a0b4b7',
     colorTwo: '#dbe7e9',
     colorThree: '#bfd3d6',
   };
 
+  validFileExtensions: ValidFileExtension[] = ['png', 'jpg', 'jpeg'];
+  validMimeTypes: ValidMimeType[] = ['image/png', 'image/jpg', 'image/jpeg'];
+
+  form!: FormGroup;
+  userRoleSub!: Subscription;
+
   constructor(private authService: AuthService) {}
 
   ngOnInit() {
-    this.authService.userRole.subscribe((role: Role | null) => {
-      role ? (this.bannerColors = this.getBannerColors(role)) : null;
+    this.form = new FormGroup({
+      file: new FormControl(null),
     });
+
+    this.userRoleSub = this.authService.userRole.subscribe(
+      (role: Role | null) => {
+        role ? (this.bannerColors = this.getBannerColors(role)) : null;
+      }
+    );
+  }
+
+  onFileSelect(event: Event) {
+    console.log('selected');
   }
 
   private getBannerColors(role: Role): BannerColors {
@@ -47,5 +68,9 @@ export class ProfileSummaryComponent implements OnInit {
       default:
         return this.bannerColors;
     }
+  }
+
+  ngOnDestroy(): void {
+    this.userRoleSub ? this.userRoleSub.unsubscribe() : null;
   }
 }

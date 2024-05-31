f2341e5c860f74cdc0384de69bf7aba77c5ad869
AlexLeoChumak <chumak1106@gmail.com>
2024-05-31 14:04:26 +0300

    create getConnectionUser(id: number)

--- src/app/home/services/connection-profile.service.ts
+++ src/app/home/services/connection-profile.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable, catchError, throwError } from 'rxjs';
+import { User } from 'src/app/auth/models/user.interface';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ConnectionProfileService {
+  private httpOptions: { headers: HttpHeaders } = {
+    headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
+  };
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  getConnectionUser(id: number): Observable<User> {
+    return this.http.get<User>(`${environment.baseApiUrl}/user/${id}`).pipe(
+      catchError((err) => {
+        console.error(err);
+        return throwError(() => err);
+      })
+    );
+  }
 }

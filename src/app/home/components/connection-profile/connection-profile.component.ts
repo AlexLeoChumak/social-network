b63e471926562d5370a7fbc0a9c2c714980bb92b
AlexLeoChumak <chumak1106@gmail.com>
2024-05-31 14:05:01 +0300

    create private getUserIdFromUrl()

--- src/app/home/components/connection-profile/connection-profile.component.ts
+++ src/app/home/components/connection-profile/connection-profile.component.ts
@@ -1,14 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { BannerColorService } from '../../services/banner-color.service';
+import { ConnectionProfileService } from '../../services/connection-profile.service';
+import { ActivatedRoute, Params, UrlSegment } from '@angular/router';
+import { Observable, map, switchMap } from 'rxjs';
+import { User } from 'src/app/auth/models/user.interface';
 
 @Component({
   selector: 'app-connection-profile',
   templateUrl: './connection-profile.component.html',
   styleUrls: ['./connection-profile.component.scss'],
 })
-export class ConnectionProfileComponent  implements OnInit {
-
-  constructor() { }
+export class ConnectionProfileComponent implements OnInit {
+  constructor(
+    public bannerColorService: BannerColorService,
+    private connectionProfileService: ConnectionProfileService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {}
 
+  private getUserIdFromUrl(): Observable<number> {
+    return this.route.params.pipe(map((params: Params) => +params['id']));
+  }
 }

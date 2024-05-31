6574b27ecd0d43958fc079ee138112c099df2300
AlexLeoChumak <chumak1106@gmail.com>
2024-05-31 16:26:25 +0300

    create FriendRequest

---
+++ src/app/home/models/friend-request.interface.ts
@@ -0,0 +1,17 @@
+export type FriendRequestStatusType =
+  | 'not-sent'
+  | 'pending'
+  | 'accepted'
+  | 'declined'
+  | 'waiting-for-current-user-response';
+
+export interface FriendRequestStatus {
+  status: FriendRequestStatusType;
+}
+
+export interface FriendRequest {
+  id: number;
+  creator: number;
+  receiver: number;
+  status?: FriendRequestStatusType;
+}

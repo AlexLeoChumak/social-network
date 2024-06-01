098c107bee62d411ff6ea0434549bb072d8e7001
AlexLeoChumak <chumak1106@gmail.com>
2024-06-01 15:32:52 +0300

    refactoring routing

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -3,15 +3,19 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomePage } from './home.page';
 import { ConnectionProfileComponent } from './components/connection-profile/connection-profile.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomePage,
-  },
-  {
-    path: ':id',
-    component: ConnectionProfileComponent,
+    children: [
+      { path: '', component: UserProfileComponent },
+      {
+        path: ':id',
+        component: ConnectionProfileComponent,
+      },
+    ],
   },
 ];
 

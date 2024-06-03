5b7d31ab3c015c320ae2144ef1f59cf5d6ea137e
AlexLeoChumak <chumak1106@gmail.com>
2024-06-03 12:06:07 +0300

    create getFriendRequestWithRenderingUsersInPopover()

--- src/app/home/components/header/friend-requests-popover/friend-requests-popover.component.ts
+++ src/app/home/components/header/friend-requests-popover/friend-requests-popover.component.ts
@@ -1,14 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { PopoverController } from '@ionic/angular';
+import { Subscription, forkJoin, from, map, mergeMap, of, tap } from 'rxjs';
+
+import { User } from 'src/app/auth/models/user.interface';
+import { FriendRequest } from 'src/app/home/models/friend-request.interface';
+import { ConnectionProfileService } from 'src/app/home/services/connection-profile.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-friend-requests-popover',
   templateUrl: './friend-requests-popover.component.html',
   styleUrls: ['./friend-requests-popover.component.scss'],
 })
-export class FriendRequestsPopoverComponent  implements OnInit {
+export class FriendRequestsPopoverComponent implements OnInit, OnDestroy {
+  friendRequests: FriendRequest[] = [];
 
-  constructor() { }
+  private getFriendRequestSub!: Subscription;
 
-  ngOnInit() {}
+  constructor(
+    public connectionProfileService: ConnectionProfileService,
+    private popoverController: PopoverController
+  ) {}
 
+  ngOnInit() {
+    this.getFriendRequestWithRenderingUsersInPopover();
+  }
+
+  getFriendRequestWithRenderingUsersInPopover() {
+    this.getFriendRequestSub = this.connectionProfileService
+      .getFriendRequest()
+      .pipe(
+        mergeMap((requests: FriendRequest[]) => from(requests)), // Преобразуем массив запросов в последовательные эмиссии
+        mergeMap((request: FriendRequest) => {
+          const creatorId = (request as any)?.creator?.id;
+
+          if (creatorId) {
+            return this.connectionProfileService
+              .getConnectionUser(creatorId)
+              .pipe(
+                tap((user: User) => {
+                  request['fullImagePath'] = `${
+                    environment.baseApiUrl
+                  }/feed/image/${user.imagePath || 'default-user-image.jpg'}`;
+                  this.friendRequests.push(request);
+                })
+              );
+          } else {
+            return of(null); // Возвращаем пустой Observable, если нет creatorId
+          }
+        })
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.getFriendRequestSub ? this.getFriendRequestSub.unsubscribe() : null;
+  }
 }
